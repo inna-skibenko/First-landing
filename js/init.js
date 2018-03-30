b61fc30ccaa38098156fca7bac8846c200bc4ff6
inna-skibenko <innochka0291@gmail.com>
2018-03-30 19:01:16 +0200

    сделали блоки Reviews, contacts, footer

--- js/init.js
+++ js/init.js
@@ -2,4 +2,12 @@ $( function() {
     // Init tabs
     $( "#tabs" ).tabs();
     $( "#tabs-portfolio" ).tabs();
+
+    // Init slick carousel  
+   $('.reviews-carousel').slick({
+     dots: true,
+     prevArrow: '<div class="slick-prev slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-left"></i></div></div>',
+     nextArrow: '<div class="slick-next slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-right"></i></div></div>'
+   });
   });
+
