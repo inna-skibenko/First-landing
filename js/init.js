69973bf95efd1a8b4f69420936147df4d1b9fc33
inna-skibenko <innochka0291@gmail.com>
2018-04-09 01:16:30 +0200

    финальный лендинг

--- js/init.js
+++ js/init.js
@@ -9,5 +9,11 @@ $( function() {
      prevArrow: '<div class="slick-prev slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-left"></i></div></div>',
      nextArrow: '<div class="slick-next slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-right"></i></div></div>'
    });
+
+   // Init slick nav
+  $('.header-nav').slicknav({
+    appendTo: '.header .container-fluid',
+    label: ''
+  });
   });
 
